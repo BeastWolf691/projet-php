a7f183aa070d68d558b338b643b4d302e35ce8cd
Steven Riviere <sriver@hotmail.fr>
2024-04-25 16:40:11 +0200

    best

--- src/Home.php
+++ src/Home.php
@@ -1,42 +1,48 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><!--lien de bootstrap-->
-    <link href="..//src/styleProject.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="..//src/styleProject.css" rel="stylesheet" type="text/css" media="all" />
     <title>GT Hôtel</title>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Accueil</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="register.html">Connexion</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Room-list.php">Chambres</a>
-        </li>
+    <nav class="navbar navbar-expand-xl bg-body-tertiary">
+        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="light dark">
+
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Menu</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="register.html">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Room-list.php">Chambres</a>
+                        </li>
 
 
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </nav>
 
         <img src="..//src/img/5451_ho_00_p_2048x1536.jpg">;
-<!--refaire la page d'accueil-->
-    <p class="profil">Vous n'avez pas de compte ? <a href="register.html">S'inscrire maintenant</a>.</p>
+        <!--refaire la page d'accueil-->
+        <p class="profil">Vous n'avez pas de compte ? <a href="register.html">S'inscrire maintenant</a>.</p>
 </body>
+
 </html>
