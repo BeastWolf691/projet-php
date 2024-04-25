730c9bb15f925aab3459cdd3e8ca7fa328931e75
Steven Riviere <sriver@hotmail.fr>
2024-04-25 15:05:55 +0200

    best

---
+++ src/register.php
@@ -0,0 +1,16 @@
+<?php
+// register.php
+
+require_once "bootstrap.php";
+use Entity\User;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $user = new User();
+    $user->setEmail($_POST['email']);
+    $user->setPassword(password_hash($_POST['password'], PASSWORD_DEFAULT));
+
+    $entityManager->persist($user);
+    $entityManager->flush();
+
+    echo "User registered with ID " . $user->getId();
+}
