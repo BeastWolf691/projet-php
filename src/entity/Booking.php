42c8c3223a4535f48d81b0f5c03fae22fb0afcbe
Steven Riviere <sriver@hotmail.fr>
2024-04-26 13:28:08 +0200

    best

---
+++ src/entity/Booking.php
@@ -0,0 +1,35 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+#[ORM\Entity]
+#[ORM\Table]
+
+class Booking
+{
+    // ... properties including references to the User and Room entities ...
+
+
+[ORM\ManyToOne(targetEntity="User")]
+[ORM\JoinColumn(name="user_id", referencedColumnName="id")]
+
+    private $user;
+
+
+[ORM\ManyToOne(targetEntity="Room")]
+[ORM\JoinColumn(name="room_id", referencedColumnName="id")]
+
+    private $room;
+
+[ORM\Column(type="datetime")]
+
+    private $checkIn;
+
+
+[ORM\Column(type="datetime")]
+
+    private $checkOut;
+
+    // ... getters and setters ...
+}
